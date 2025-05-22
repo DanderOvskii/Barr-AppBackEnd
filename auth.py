2c9d0d5ec410969131023196af7b28948a84987f
dander siegers <89800@glr.nl>
2025-05-22 23:49:00 +0200

    improved token verefication

--- auth.py
+++ auth.py
@@ -3,6 +3,9 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session
+from database import get_session
+from models import User
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -18,10 +21,9 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-def create_access_token(user_id: int, is_admin: bool):
+def create_access_token(user_id: int):
     to_encode = {
-        "sub": str(user_id),
-        "is_admin": is_admin
+        "sub": str(user_id)
         }
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
@@ -32,17 +34,45 @@ def verify_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id: str = payload.get("sub")
-        is_admin: bool = payload.get("is_admin",False)
         if user_id is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Could not validate credentials",
                 headers={"WWW-Authenticate": "Bearer"},
             )
-        return {"user_id": int(user_id), "is_admin": is_admin}
+        return {"user_id": int(user_id)}
     except JWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    
+def token_required(token: str = Depends(oauth2_scheme)) -> None:
+    verify_token(token)
+    # If invalid, verify_token will raise an HTTPException
+    return None
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    session: Session = Depends(get_session)
+) -> User:
+    data = verify_token(token)
+    user_id = data["user_id"]
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    print(user)
+    return user
+
+def get_current_admin(current_user: User = Depends(get_current_user)) -> User:
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Admins only"
+        )
+    return current_user.is_admin
+   
--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from database import get_session, init_db
 from crud import  get_products,get_categories,get_categories_with_products,update_product,create_product_db,delete_product,search_products,create_user,get_user_by_username,authenticate_user,get_user_stats,update_user,create_category,delete_category
 from models import Products,Categories,CategoryWithProducts,User,UserUpdate
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from auth import create_access_token, verify_token
+from auth import create_access_token, token_required, get_current_user, get_current_admin
 from datetime import date
 
 
 app = FastAPI()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8081"],  # Adjust this to your frontend's origin
+    allow_origins=["exp://192.168.1.99:8081",  # Expo Go app
+        "http://localhost:8081", ],  # Adjust this to your frontend's origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -23,44 +24,7 @@ app.add_middleware(
 def on_startup():
     init_db()
 
-@app.get("/Products/", response_model=list[Products])
-def read_items(categoryId: int = None, session: Session = Depends(get_session)):
-    return get_products(session, categoryId)
-
-@app.get("/", response_model=list[Categories])  # New endpoint to get all items
-def read_categories(session: Session = Depends(get_session)):
-    return get_categories(session)
-
-
-@app.get("/ProductManager/", response_model=list[CategoryWithProducts])
-def read_categories_with_products(session: Session = Depends(get_session)):
-    return get_categories_with_products(session)
-
-@app.put("/ProductManager/{productId}", response_model=Products)
-def update_item(productId: int, updated_product: Products, session: Session = Depends(get_session)):
-    return update_product(session, productId, updated_product)
-
-@app.post("/ProductManager/", response_model=Products, status_code=status.HTTP_201_CREATED)
-def create_product(product: Products, session: Session = Depends(get_session)):
-    try:
-        return create_product_db(session, product)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    
-@app.delete("/ProductManager/{productId}")
-def delete_item(productId: int, session: Session = Depends(get_session)):
-    try:
-        return delete_product(session, productId)
-    except ValueError as e:
-        raise HTTPException(status_code=404, detail=str(e))
-
-
-@app.get("/search", response_model=list[Products])
-def search_items(q: str, session: Session = Depends(get_session)):
-    results = search_products(session, q)
-    print('Search Results:', results)  # Print the data
-    return results
-
+#LOGIN REGESTRATION FUNCTIONS//////////////////////////////////////////////////////////////////////////////////////////////////////
 @app.post("/register")
 def register_user(
     username: str,
@@ -73,7 +37,7 @@ def register_user(
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
     user = create_user(session, username, password,birthdate, is_admin)
-    access_token = create_access_token(user_id=user.id, is_admin=user.is_admin)
+    access_token = create_access_token(user_id=user.id)
     return {"access_token": access_token, "token_type": "bearer"}
 
 @app.post("/token")
@@ -88,58 +52,70 @@ def login(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token = create_access_token(user_id=user.id, is_admin=user.is_admin)
+    access_token = create_access_token(user_id=user.id)
     return {"access_token": access_token, "token_type": "bearer"}
+#LOGIN REGESTRATION FUNCTIONS//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@app.get("/", response_model=list[Categories])  # New endpoint to get all items
+def read_categories(
+    _: None = Depends(token_required),
+    session: Session = Depends(get_session),
+    ):
+    return get_categories(session)
+
+@app.get("/Products/", response_model=list[Products])
+def read_items(
+    categoryId: int = None, 
+    _: None = Depends(token_required),
+    session: Session = Depends(get_session),
+    ):
+    return get_products(session, categoryId)
 
 
 
-@app.get("/verify-token/{token}")
-async def verify_token_token(token: str):
-    verify_token(token=token)
-    return {"token": token}
 
-@app.get("/users/me", response_model=dict)
-async def get_current_user(
-    token: str = Depends(oauth2_scheme),
-    session: Session = Depends(get_session)
-):
-    try:
-        user_id = verify_token(token)
-        user = session.get(User, user_id["user_id"])
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="User not found"
-            )
-        user_stats = get_user_stats(session, user.id)
-        return {
-            "id": user.id,
-            "username": user.username,
-            "wallet": user_stats.wallet if user_stats else 0.0,
-            "isAdmin": user.is_admin   
-        }
-    except HTTPException as e:
-        raise e
+
+@app.get("/search", response_model=list[Products])
+def search_items(
+    q: str, 
+    _: None = Depends(token_required),
+    session: Session = Depends(get_session),
+    ):
+    results = search_products(session, q)
+    print('Search Results:', results)  # Print the data
+    return results
+
+
+
+
+
+
+@app.get("/users/me")
+async def get_user_info(
+    current_user: User = Depends(get_current_user),
+    session: Session = Depends(get_session),
+    ):
+    user_stats = get_user_stats(session=session, user_id=current_user.id)
+    return {
+        "id": current_user.id,
+        "username": current_user.username,
+        "wallet": user_stats.wallet if user_stats else 0.0,
+        "isAdmin": current_user.is_admin
+    }
     
 @app.post("/buy/{product_id}")
 async def buy_product(
     product_id: int,
-    token: str = Depends(oauth2_scheme),
+    current_user: User = Depends(get_current_user),
     session: Session = Depends(get_session)
 ):
-    # Verify user
-    user_id = verify_token(token)
-    user = session.get(User, user_id["user_id"])
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    
     # Get product
     product = session.get(Products, product_id)
     if not product:
         raise HTTPException(status_code=404, detail="Product not found")
     
     # Get user stats
-    user_stats = get_user_stats(session, user.id)
+    user_stats = get_user_stats(session, current_user.id)
     if not user_stats:
         raise HTTPException(status_code=404, detail="User stats not found")
     
@@ -161,30 +137,20 @@ async def buy_product(
 @app.put("/users/update", response_model=dict)
 async def update_user_endpoint(
     user_data: dict,
-    token: str = Depends(oauth2_scheme),
+    current_user: User = Depends(get_current_user),
     session: Session = Depends(get_session)
 ):
-    try:
-        # Verify token and get username
-        user_id = verify_token(token)
-        
-        # Update user
-        updated_user = update_user(session, user_id["user_id"], user_data)
-        return updated_user
+
+    # Update user
+    updated_user = update_user(session, current_user.id, user_data)
+    return updated_user
     
-    except ValueError as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(e)
-        )
-    except HTTPException as e:
-        raise e
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="An error occurred while updating user data"
-        )
+
     
+
+    
+
+#PRODUCT MANAGER FUNCTIONS//////////////////////////////////////////////////////////////////////////////////////////////////////
 @app.post("/ProductManager/categories", response_model=Categories, status_code=status.HTTP_201_CREATED)
 def create_category_endpoint(category: Categories, session: Session = Depends(get_session)):
     try:
@@ -198,4 +164,36 @@ def delete_category_endpoint(categoryId: int, session: Session = Depends(get_ses
         return delete_category(session, categoryId)
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
+    
+    
+@app.get("/ProductManager/", response_model=list[CategoryWithProducts])
+def read_categories_with_products(
+    _: None = Depends(get_current_admin),
+    session: Session = Depends(get_session),
+):
+    return get_categories_with_products(session)
+
+@app.put("/ProductManager/{productId}", response_model=Products)
+def update_item(productId: int, updated_product: Products, session: Session = Depends(get_session)):
+    return update_product(session, productId, updated_product)
+
+@app.post("/ProductManager/", response_model=Products, status_code=status.HTTP_201_CREATED)
+def create_product(product: Products, session: Session = Depends(get_session)):
+    try:
+        return create_product_db(session, product)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+@app.delete("/ProductManager/{productId}")
+def delete_item(productId: int, session: Session = Depends(get_session)):
+    try:
+        return delete_product(session, productId)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+#PRODUCT MANAGER FUNCTIONS//////////////////////////////////////////////////////////////////////////////////////////////////////
+#ACCOUNT MANAGER FUNCTIONS//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#ACCOUNT MANAGER FUNCTIONS//////////////////////////////////////////////////////////////////////////////////////////////////////
+
     
